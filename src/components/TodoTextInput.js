aa97879246875cd3f19f55e41566068eeb17c842
Inna Golovina <igigig978@gmail.com>
2021-07-19 12:34:16 +0300

    init

---
+++ src/components/TodoTextInput.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+
+export default class TodoTextInput extends Component {
+  static PropTypes = {
+    onSave: 
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
